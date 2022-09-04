1b9c3ab6289ff6bbf9f061fccc5d02765f1d5918
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-09-04 23:29:45 +0300

    adding loading msg

--- fade-to-clear-loading-screen/app.js
+++ fade-to-clear-loading-screen/app.js
@@ -1,5 +1,6 @@
 const background = document.querySelector('.bg');
 const counter = document.querySelector('.loading-text');
+const loadedMsg = document.querySelector('.loaded')
 
 let load = 0;
 const interval = setInterval(loading, 30);
@@ -9,12 +10,15 @@ function loading() {
 
     if (load > 99) {
         clearInterval(interval);
+
+        loadedMsg.style.display = 'block';
     }
 
     counter.textContent = `${load}%`
     counter.style.opacity = scale(load, 0, 100, 1, 0);
     background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
+
 
 // I got the next scale func from https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (number, inMin, inMax, outMin, outMax) => {
