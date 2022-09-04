c583a3ca4c4024592dc29939f0e2f71a9ed55264
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-09-04 23:15:21 +0300

    added js code

---
+++ fade-to-clear-loading-screen/app.js
@@ -0,0 +1,22 @@
+const background = document.querySelector('.bg');
+const counter = document.querySelector('.loading-text');
+
+let load = 0;
+const interval = setInterval(loading, 30);
+
+function loading() {
+    load++;
+
+    if (load > 99) {
+        clearInterval(interval);
+    }
+
+    counter.textContent = `${load}%`
+    counter.style.opacity = scale(load, 0, 100, 1, 0);
+    background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+// I got the next scale func from https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
