3347cd060efd2044f524dc423a391943ef230bac
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-10-07 15:48:27 +0300

    adding form and styles

--- multi-step-login/src/App.tsx
+++ multi-step-login/src/App.tsx
@@ -1,9 +1,42 @@
+import { useMultiStepForm } from "./useMultiStepForm"
 
 
 function App() {
-
+  const {steps, currentStepIndex, step, isFirstStep, isLastStep, back, next} = useMultiStepForm([
+    <div>One</div>,
+    <div>Two</div>
+  ])
   return (
-   <h1>Hi</h1>
+   <div style={{
+    position: 'relative',
+    background: 'white',
+    border: '1px solid black',
+    padding: '2rem',
+    margin: '1rem',
+    borderRadius: '.5rem',
+    fontFamily: 'Arial',
+   }}>
+    <form>
+      <div style={{
+        position: 'absolute',
+        top: '.5rem',
+        right: '.5rem'
+      }}>
+        {currentStepIndex + 1} / {steps.length}
+      </div>
+      {step}
+      <div style={{
+        marginTop: '1rem',
+        display: 'flex',
+        gap: '.5rem',
+        justifyContent: 'flex-end',
+      }}>
+        {!isFirstStep && <button type="button" onClick={back}>Back</button>}
+        <button type="button" onClick={next}>{!isLastStep ? "Next" : "Finish"}</button>
+
+      </div>
+    </form>
+   </div>
   )
 }
 
